9210d53c5414b1e697af38c16558c0e894ed4216
serendip <serendip811@gmail.com>
2017-02-27 12:53:40 +0900

    임시 처리 mecab...

--- services/pos.js
+++ services/pos.js
@@ -6,6 +6,18 @@ exports.nouns = function(text, callback){
     mecab.pos(text, function(err, result){
     	var nouns = [];
         console.log("pos : ", result);
+        if(typeof result === 'undefined') {
+            // mecab 이 제대로 안되면 ㅠ_ㅠ
+            if(text.indexOf('커피') >= 0){
+                nouns.push('커피')
+            }
+            if(text.indexOf('메뉴') >= 0){
+                nouns.push('메뉴')
+            }
+            if(text.indexOf('주식') >= 0){
+                nouns.push('주식')
+            }
+        }
 
     	if(result){
             console.log(result);
