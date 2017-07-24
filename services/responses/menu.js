4f6b2c7b25c8541b80830d94e348c2d908482d6b
김예준 <serendip@admin-ui-MacBook-Pro-2.local>
2017-07-24 14:35:01 +0900

    식사 시간별로 default

--- services/responses/menu.js
+++ services/responses/menu.js
@@ -10,11 +10,18 @@ var moment = require('moment');
 exports.getResponses = function(intent, entities, context, callback){
 	console.log("menu getResponses");
 
-	var my_callback = function(intent, entities, message){
+	var my_callback = function(intent, entities, message, time_meal){
 		var response = {};
 		response.intent = intent;
 		response.entities = entities; // new_entities를 넣어준다.
 		response.message = message; // new_entities를 넣어준다.
+		if(time_meal == "breakfast") {
+			response.buttons = ["점심 식사 메뉴도 알려주세요", "저녁 식사 메뉴는요?" ,"처음으로 돌아갈게요."];	
+		} else if(time_meal == "lunch") {
+			response.buttons = ["아침 식사 메뉴도 알려주세요", "저녁 식사 메뉴는요?" ,"처음으로 돌아갈래요."];	
+		} else {
+			response.buttons = ["아침 식사 메뉴도 알려주세요", "점심 식사 메뉴는요?" ,"처음으로 돌아갑시다요."];	
+		}
 
 		callback(response);
 	}
@@ -28,12 +35,12 @@ exports.getResponses = function(intent, entities, context, callback){
 		var date = '';
 		var time_meal = '';
 		for (var i = new_entities.length - 1; i >= 0; i--) {
-			if(typeof new_entities[i].value === 'undefined') {
-				// TODO : 이거 응답하는 메시지도 없을때 뭐라고 return할지 entity에 넣어두면 좋겠오
-				message = "'" + new_entities[i].name + "'를 알려주세요!";
-				my_callback(intent, new_entities, message);
-				return ;	// 끝
-			}
+			// if(typeof new_entities[i].value === 'undefined') {
+			// 	// TODO : 이거 응답하는 메시지도 없을때 뭐라고 return할지 entity에 넣어두면 좋겠오
+			// 	message = "'" + new_entities[i].name + "'를 알려주세요!";
+			// 	my_callback(intent, new_entities, message);
+			// 	return ;	// 끝
+			// }
 
 			switch (new_entities[i].key){
 				case 'date' :
@@ -45,6 +52,22 @@ exports.getResponses = function(intent, entities, context, callback){
 			}
 		}
 
+		if(date == null || date == '' || date == undefined){
+			date = 'today';
+		}
+		if(time_meal == null || time_meal == '' || time_meal == undefined){
+			if(moment().format('HH') < 10) {
+				time_meal = 'breakfast';
+			} else if(moment().format('HH') < 14){
+				time_meal = 'lunch';
+			} else if(moment().format('HH') < 20){
+				time_meal = 'dinner';
+			} else {
+				time_meal = 'lunch';
+			}
+
+		}
+
 		// // TODO: 
 		// // 해당 response가 사용자의 의도와 얼마나 부합하는지 확률도 같이 보내줘서 selector가 판별하게 할 수 있을까?
 		// // 는 나중에 하자 -.-
@@ -54,7 +77,7 @@ exports.getResponses = function(intent, entities, context, callback){
 	    	case 'today' :
 	    		date = moment().format('YYYY-MM-DD');
 	    		break;
-	    	case 'yesterdat' :
+	    	case 'yesterday' :
 	    		date = moment().subtract(1, 'days').format('YYYY-MM-DD');
 	    		break;
 	    	case 'tomorrow' :
@@ -87,7 +110,7 @@ exports.getResponses = function(intent, entities, context, callback){
 				}
 				message += "\n식사 맛있게 하십쇼 (__)";
 
-				my_callback(intent, new_entities, message);
+				my_callback(intent, new_entities, message, time_meal);
 			});
 		}
 
